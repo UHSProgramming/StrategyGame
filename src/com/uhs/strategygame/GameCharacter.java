d506fb0e58085de598b5966684e1df9a85744d31
Ununhexium116 <ununhaxium@gmail.com>
2017-11-21 14:44:31 -0500

    Change names on GameCharacter and GameEnemy classes

--- src/com/uhs/strategygame/GameCharacter.java
+++ src/com/uhs/strategygame/GameCharacter.java
@@ -2,8 +2,8 @@ package com.uhs.strategygame;
 
 import java.util.List;
 
-public abstract class Character {
+public abstract class GameCharacter {
     abstract List<String> getAttacks();//get possible attacks that the character can perform
-    abstract void doAttack(String attack, int posx, int posy, List<Character> characters);//perform an attack
+    abstract void doAttack(String attack, int posx, int posy, List<GameCharacter> gameCharacters);//perform an attack
     abstract boolean receivedDamage(float damage);//acquire damage
 }
