65ecffcf8ecfe242c79f2d6b5d5cacf395c2f98b
Ben <ben.greenberg@sixgables.com>
2017-11-29 18:18:35 -0500

    welcome PYTHON

---
+++ README.md
@@ -0,0 +1,2 @@
+# StrategyGame
+The game created by the Urbana High School programming club
---
+++ src/Main.py
@@ -0,0 +1,32 @@
+import pygame
+import sys
+
+
+pygame.init() # initialize pygame
+
+winSize = (800, 600)
+screen = pygame.display.set_mode(winSize)
+pygame.display.set_caption("Strategy Game - UHS Programming")
+
+running = True
+clock = pygame.time.Clock()
+
+# misc temporary variables
+BACKGROUND_COLOR = (255, 0, 128)
+testImage = pygame.image.load("../res/img/lyn.png")
+
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: # user quit?
+            running = False
+
+    screen.fill(BACKGROUND_COLOR) # draw the background
+
+    screen.blit(testImage, (100,100))
+    
+    pygame.display.flip() # flip the double buffer
+    clock.tick(60) # move the clock forward
+
+pygame.quit() # quit pygame
+        
